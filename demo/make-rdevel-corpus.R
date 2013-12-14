b1de3c385e62e652cd023050dddee92f9e3aa0a3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-12-14 01:13:58 -0800

    Most of the examples in the paper work now

--- demo/make-rdevel-corpus.R
+++ demo/make-rdevel-corpus.R
@@ -35,5 +35,58 @@ print(
     lines = "both"
   )
 )
+
+# data cleaning
+require(tm)
+old.mc.cores <- options(mc.cores = 1) # debugging
+rdevel.corpus <- tm_map(rdevel.corpus, as.PlainTextDocument)
+rdevel.corpus <- tm_map(rdevel.corpus, stripWhitespace)
+rdevel.corpus <- tm_map(rdevel.corpus, tolower)
+rdevel.corpus <- tm_map(rdevel.corpus, stemDocument, language='english')
+#rdevel.corpus <- tm_map(rdevel.corpus, removeWords, stopwords('english'))
+
 print(summary(rdevel.corpus))
 save(rdevel.corpus, file='/home/Email/rdevel-corpus.rda', compress='xz')
+
+# Authors
+authors <- lapply(rdevel.corpus, Author)
+authors <- sapply(authors, paste, collapse = " ")
+print(sort(table(authors), decreasing = TRUE)[1:5])
+
+# Topics / Headings
+headings <- lapply(rdevel.corpus, Heading)
+headings <- sapply(headings, paste, collapse = " ")
+
+# The sorted contingency table shows the biggest topics’ names and the amount of postings
+print(bigTopicsTable <- sort(table(headings), decreasing = TRUE)[1:5])
+bigTopics <- names(bigTopicsTable)
+
+# First topic
+topicCol <- rdevel.corpus[headings == bigTopics[1]]
+print(unique(sapply(topicCol, Author)))
+
+# Second topic
+topicCol <- rdevel.corpus[headings == bigTopics[2]]
+print(unique(sapply(topicCol, Author)))
+
+# How many postings deal with bugs?
+print(
+  bugCol <- tm_filter(
+    rdevel.corpus,
+    FUN = searchFullText,
+    "[^[:alpha:]]+bug[^[:alpha:]]+",
+    doclevel = TRUE
+  )
+)
+
+# Most active authors about bugs
+bugColAuthors <- lapply(bugCol, Author)
+bugColAuthors <- sapply(bugColAuthors, paste, collapse = " ")
+print(sort(table(bugColAuthors), decreasing = TRUE)[1:5])
+
+# now make Term-Document Matrix
+tdm <- TermDocumentMatrix(rdevel.corpus)
+
+# find frequent terms
+f <- findFreqTerms(tdm, 30, 31)
+print(sort(f[-grep("[0-9]", f)]))
