a167b274150d0480f3da5c3f7ca922a30fe91ad0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-12-19 12:43:13 -0800

    make one corpus for each mailbox!

---
+++ demo/get-enron-corpora.R
@@ -0,0 +1,38 @@
+library(downloader)
+library(email.mining.for.journos)
+library(tm)
+here <- setwd('~/Downloads')
+directory <- 'enron_mail_20110402'
+tarball <- paste(directory, 'tgz', sep = '.')
+tarball.url <- paste('http://download.srv.cs.cmu.edu/~enron', tarball, sep = '/')
+unlink(directory, recursive = TRUE, force = TRUE)
+unlink(tarball, force = TRUE)
+download(
+  url = tarball.url,
+  destfile = tarball,
+  mode = 'wb'
+)
+untar(tarball, compressed='gzip')
+setwd(paste(directory, 'maildir', sep = '/'))
+mailboxes <- unique(
+  sub(pattern = '\\/[1-9][0-9]*\\.$', replacement = '',
+    grep(pattern = '.',
+      list.files(full.names=FALSE, recursive=TRUE),
+      fixed=TRUE,
+      value=TRUE)
+  )
+)
+for (mailbox.name in mailboxes) {
+  print(paste('Processing mailbox', mailbox.name, sep = ' '))
+  email.corpus <- corpus.from.eml(mailbox.name)
+  meta(email.corpus, tag = 'creator', type = 'corpus') <- '@znmeb'
+  meta(email.corpus, tag = 'mailbox.name', type = 'corpus') <- mailbox.name
+  print(summary(email.corpus))
+  save.name <- gsub(pattern = '/', replacement = '-', mailbox.name)
+  save(
+    email.corpus,
+    file = paste(save.name, 'corpus.rda', sep = '.'),
+    compress = 'xz'
+  )
+}
+setwd(here)
