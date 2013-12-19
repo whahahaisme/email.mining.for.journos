ce4dca9ec4cfeb0199b68a06d5e69720536c3858
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-12-19 02:18:46 -0800

    factored out the corpus build

--- demo/get-r-mailing-lists.R
+++ demo/get-r-mailing-lists.R
@@ -1,20 +1,16 @@
-library(downloader)
-library(tm)
-library(tm.plugin.mail)
+library(email.mining.for.journos)
 dir.create(path = '~/Downloads/RMailingLists', recursive = TRUE, mode = '0755')
 here <- setwd('~/Downloads/RMailingLists')
 for (mailing.list in c('r-devel', 'r-help')) {
   dir.create(path = mailing.list)
   setwd(mailing.list)
   where <- paste('https://stat.ethz.ch/pipermail', mailing.list, sep = '/')
-
   download(
     url = where,
     destfile = 'webpage.html',
     mode = 'wb',
     extra = '--no-check-certificate'
   )
-
   file.names <- sub(
     'gz.*$',
     'gz',
@@ -27,43 +23,19 @@ for (mailing.list in c('r-devel', 'r-help')) {
 
   for (source.file in file.names) {
     source.url <- paste(where, source.file, sep = '/')
-
     download(
       url = source.url,
       destfile = source.file,
       mode = 'wb',
       extra = '--no-check-certificate'
     )
-
-    # now make and save a corpus!
-    directory <- sub('.txt.gz', '', source.file, fixed=TRUE)
-
-    convert_mbox_eml(
-      source.file,
-      directory
-    )
-
-    email.corpus <- VCorpus(
-      DirSource(directory),
-      readerControl = list(
-        reader = readMail,
-        load = TRUE
-      )
-    )
-
+    email.corpus <- corpus.from.mbox(source.file)
     save(
       email.corpus,
-      file = paste(
-        directory,
-        '.corpus.rda',
-        sep=''
-      ),
+      file = sub('txt.gz', 'corpus.rda', source.file),
       compress = 'xz'
     )
-
   }
-
   setwd('..')
 }
-
 setwd(here)
