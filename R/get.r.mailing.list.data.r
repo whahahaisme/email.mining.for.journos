b9662013a23a4f8e72c478713befa87aa74cffe5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-12-25 00:51:45 -0800

    coded the corpus builder

--- R/get.r.mailing.list.data.r
+++ R/get.r.mailing.list.data.r
@@ -15,13 +15,13 @@
 #' @importFrom downloader download
 #' @param destination.directory absolute path to a directory where you want the downloaded Enron corpus stored
 #' @examples
-#' # mailing.list.root <- 
+#' # r.mailing.list.root <- 
 #' #   download.r.mailing.list.archives(
 #' #     destination.directory = '/data/RMailingLists'
 #' # )
 
 download.r.mailing.list.archives <- function(destination.directory) {
-  mailing.list.root <- 'https://stat.ethz.ch/pipermail'
+  r.mailing.list.root <- 'https://stat.ethz.ch/pipermail'
   
   print(paste('Removing', destination.directory))
   unlink(destination.directory, recursive = TRUE, force = TRUE)
@@ -34,7 +34,7 @@ download.r.mailing.list.archives <- function(destination.directory) {
   for (mailing.list in c('r-devel', 'r-help')) {
     dir.create(path = mailing.list)
     setwd(mailing.list)
-    where <- paste(mailing.list.root, mailing.list, sep = '/')
+    where <- paste(r.mailing.list.root, mailing.list, sep = '/')
     download(
       url = where,
       destfile = 'webpage.html',
@@ -64,10 +64,73 @@ download.r.mailing.list.archives <- function(destination.directory) {
         extra = '--no-check-certificate'
       )
     }
+    setwd('..')
   }
-  setwd('..')
 
   print(paste('Returning to', here))
   setwd(here)
-  return(mailing.list.root)
+  return(r.mailing.list.root)
+}
+
+#' Make corpora from the R mailing list archives
+#'
+#' 'corpora.from.r.mailing.list.archives' makes corpora, one per month of archived data, from the mailboxes archives acquired via 'download.r.mailing.list.archives'
+#'
+#' @keywords email Enron corpus eml
+#' @export corpora.from.r.mailing.list.archives
+#' @importFrom tm meta
+#' @importFrom tm meta<-
+#' @param destination.directory absolute path to a directory where you want the downloaded Enron corpus stored
+#' @param r.mailing.list.root The URL of the Enron tarball. This is inserted into the metadata of the output corpora
+#' @examples
+#' # corpora.from.r.mailing.list.archives(
+#' #   '/data/RMailingLists',
+#' #   'https://stat.ethz.ch/pipermail'
+#' # )
+
+corpora.from.r.mailing.list.archives <- function(destination.directory, r.mailing.list.root) {
+  here <- setwd(destination.directory)
+  print(paste('Left', here, 'for', getwd()))
+
+  for (mailing.list in c('r-devel', 'r-help')) {
+    setwd(mailing.list)
+    
+    # get the archive file names
+    file.names <- list.files(full.names=FALSE, recursive=FALSE)
+    file.names <- grep(pattern = '.tar.gz$', file.names, value=TRUE)
+
+    for (source.file in file.names) {
+
+      print(paste('Processing archive', mailing.list, archive))
+      email.corpus <- corpus.from.mbox(
+        source.file = source.file,
+        datestampformat = '%a, %d %b %Y %X %z'
+      )
+      
+      meta(email.corpus, tag = 'creator', type = 'corpus') <- 'znmeb@znmeb.net'
+      meta(email.corpus, tag = 'source.url', type = 'corpus') <- 
+        paste(r.mailing.list.root, mailing.list, source.file, sep = '/') 
+
+      # compute save file name
+      month <- sub(pattern = '.txt.gz$', replacement = '', source.file)
+      month <- sub(pattern = '^.+-', replacement = '', month)
+      month.number <- sprintf('%02d', which(month.name == month))
+      save.name <- sub(
+        pattern = '.txt.gz$',
+        replacement = '.corpus.rda',
+        source.file
+      )
+      save.name <- sub(pattern = month, replacement = month.number, save.name)
+      save.name <- paste(mailing.list, save.name, sep = '-')
+      print(paste('Saving corpus', save.name))
+      save(
+        email.corpus,
+        file = save.name,
+        compress = 'xz'
+      )
+    }
+    setwd('..')
+  }
+  print(paste('Returning to', here))
+  setwd(here)
 }
