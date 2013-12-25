20aa49235197833d64d2eceeebdbc48e2b95b774
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-12-24 22:56:51 -0800

    renamed

--- demo/get-enron-corpora.R
+++
@@ -1,6 +0,0 @@
-#' @demoTitle Download Enron corpus tarball and build corpora from the mailboxes
-enron.tarball.url <- download.enron.mailboxes(destination.directory = '/data/Enron')
-corpora.from.enron.mailboxes(
-  destination.directory = '/data/Enron',
-  tarball.url = enron.tarball.url
-)
