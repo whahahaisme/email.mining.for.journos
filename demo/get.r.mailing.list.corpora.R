1df4ee1bb881ed9a8977b3e339e33a919f4ea034
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-12-25 10:19:16 -0800

    new calling sequence

--- demo/get.r.mailing.list.corpora.R
+++ demo/get.r.mailing.list.corpora.R
@@ -1,9 +1,8 @@
 #' @demoTitle Download R-devel / R-help archives & build corpora
 library(email.mining.for.journos)
-r.mailing.list.root <- download.r.mailing.list.archives(
+download.r.mailing.list.archives(
   destination.directory = '/data/RMailingLists'
 )
 corpora.from.r.mailing.list.archives(
-  destination.directory = '/data/RMailingLists',
-  r.mailing.list.root = r.mailing.list.root
+  destination.directory = '/data/RMailingLists'
 )
