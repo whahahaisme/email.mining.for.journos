84afe1ee6974405bf2d4e339b0936d5c67fb377e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-12-27 16:51:34 -0800

    adding some sanity to the spacing

--- demo/get.r.mailing.list.corpora.r
+++ demo/get.r.mailing.list.corpora.r
@@ -1,5 +1,7 @@
 #' @demoTitle Download R-devel / R-help archives & build corpora
 library(email.mining.for.journos)
 download.r.mailing.list.archives(destination.directory = '/data/RMailingLists')
-corpora.from.r.mailing.list.archives(destination.directory = '/data/RMailingLists',
-                                     creator = 'znmeb@znmeb.net')
+corpora.from.r.mailing.list.archives(
+  destination.directory = '/data/RMailingLists',
+  creator = 'znmeb@znmeb.net'
+)
