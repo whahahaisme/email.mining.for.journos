3d12849e844db377b418156fc0307f8acbbf9cb7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2013-12-28 11:41:40 -0800

    Testing on Windows - should also work on Linux and MacOS

--- demo/get.r.mailing.list.corpora.r
+++ demo/get.r.mailing.list.corpora.r
@@ -1,7 +1,9 @@
 #' @demoTitle Download R-devel / R-help archives & build corpora
 library(email.mining.for.journos)
-download.r.mailing.list.archives(destination.directory = '/data/RMailingLists')
+download.r.mailing.list.archives(
+  destination.directory = '~/Documents/Data/RMailingLists'
+)
 corpora.from.r.mailing.list.archives(
-  destination.directory = '/data/RMailingLists',
+  destination.directory = '~/Documents/Data/RMailingLists',
   creator = 'znmeb@znmeb.net'
 )
